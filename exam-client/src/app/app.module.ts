79b43586706a0771389bfb70f87432c181b21b0a
Kinson Poon <test@DESKTOP-Q1OIQQ1.localdomain>
2021-12-30 19:58:22 +0800

    front end setup

--- exam-client/src/app/app.module.ts
+++ exam-client/src/app/app.module.ts
@@ -4,7 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button' //added, if want to add buton in materialUI from the documentation
+import {MatButtonModule} from '@angular/material/button';
+
+
 @NgModule({
   declarations: [
     AppComponent
@@ -13,7 +15,7 @@ import {MatButtonModule} from '@angular/material/button' //added, if want to add
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
